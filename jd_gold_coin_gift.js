0ed67f5aeee144f586f9c08be7f4c42678d2f670
noevers <85549892+noevers@users.noreply.github.com>
2021-10-18 15:18:48 +0800

    Update and rename jd_gold_coin_gift to jd_gold_coin_gift.js

--- jd_gold_coin_gift.js
+++ jd_gold_coin_gift.js
@@ -10,13 +10,13 @@
 
 =================================Loon===================================
 [Script]
-cron "22 0,8 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_GoldcoinToGift.js,tag=攒金币 赢大礼
+cron "22 0,8 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_gold_coin_gift.js,tag=攒金币 赢大礼
 
 ===================================Surge================================
-攒金币 赢大礼 = type=cron,cronexp="22 0,8 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_GoldcoinToGift.js
+攒金币 赢大礼 = type=cron,cronexp="22 0,8 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_gold_coin_gift.js
 
 ====================================小火箭=============================
-攒金币 赢大礼 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_GoldcoinToGift.js, cronexpr="22 0,8 * * *", timeout=3600, enable=true
+攒金币 赢大礼 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_gold_coin_gift.js, cronexpr="22 0,8 * * *", timeout=3600, enable=true
  */
 const $ = new Env('攒金币 赢大礼');
 const notify = $.isNode() ? require('./sendNotify') : '';
