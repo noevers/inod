cd70b93cf0deb29a4f7f32fdab5202ac6c47349a
noevers <85549892+noevers@users.noreply.github.com>
2021-10-21 15:25:12 +0800

    Update jd_ccSign.js

--- jd_ccSign.js
+++ jd_ccSign.js
@@ -160,13 +160,13 @@ function getSign(functionid, body, uuid) {
       "client":"android",
       "clientVersion":"10.1.2"
     }
-    let HostArr = ['jdsign.cf', 'signer.nz.lu']
-    let Host = HostArr[Math.floor((Math.random() * HostArr.length))]
+    let Host = "sign.jdhost.cf"
     let options = {
-      url: `https://cdn.nz.lu/ddo`,
+      url: `https://sign.jdhost.cf/getCCSign`,
       body: JSON.stringify(data),
       headers: {
         Host,
+		"Content-Type": "application/json;charset=UTF-8",
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       },
       timeout: 30 * 1000
