be66ecd5877a21acf633d9060c99e3b1c379a1ad
noevers <85549892+noevers@users.noreply.github.com>
2021-10-28 21:36:31 +0800

    Update jd_1111.js

--- jd_1111.js
+++ jd_1111.js
@@ -2,7 +2,7 @@
 双十一无门槛红包🧧
 ck1助力 作者
 其余助力ck1
-https://u.jd.com/3C7eCOr
+https://u.jd.com/3KUlIt5
 跳转到app 可查看助力情况
 1 0,12,18 * * * jd_1111.js
 */
@@ -40,7 +40,7 @@ let nowTime = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*
     if ($.isNode()) await notify.sendNotify($.name + '活动已结束', `请删除此脚本\n咱江湖再见`);
     return
   }
-  $.shareCode = 'zVAej'
+  $.shareCode = '9uXKM'
   for (let i = 0; i < 1; i++) {
     cookie = cookiesArr[i];
     if (cookie) {
@@ -52,9 +52,9 @@ let nowTime = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*
     }
   }
   if(message){
-    $.msg($.name, ``, `${message}\nhttps://u.jd.com/3C7eCOr\n\n跳转到app 可查看助力情况`);
+    $.msg($.name, ``, `${message}\nhttps://u.jd.com/3KUlIt5\n\n跳转到app 可查看助力情况`);
     if ($.isNode()){
-      await notify.sendNotify(`${$.name}`, `${message}\n\nhttps://u.jd.com/3C7eCOr\n跳转到app 可查看助力情况`);
+      await notify.sendNotify(`${$.name}`, `${message}\n\nhttps://u.jd.com/3KUlIt5\n跳转到app 可查看助力情况`);
     }
   }
 })()
@@ -110,7 +110,7 @@ async function run(){
 function getCoupons(shareId = '') {
   return new Promise(resolve => {
     let opts = {
-      url: `https://api.m.jd.com/api?functionId=getCoupons&appid=u&_=${Date.now()}&loginType=2&body={%22platform%22:4,%22unionActId%22:%2231134%22,%22actId%22:%22${$.actId}%22,%22d%22:%223C7eCOr%22,%22unionShareId%22:%22${shareId}%22,%22type%22:1,%22eid%22:%22${$.eid}%22}&client=apple&clientVersion=8.3.6&h5st=undefined`,
+      url: `https://api.m.jd.com/api?functionId=getCoupons&appid=u&_=${Date.now()}&loginType=2&body={%22platform%22:4,%22unionActId%22:%2231134%22,%22actId%22:%22${$.actId}%22,%22d%22:%223KUlIt5%22,%22unionShareId%22:%22${shareId}%22,%22type%22:1,%22eid%22:%22${$.eid}%22}&client=apple&clientVersion=8.3.6&h5st=undefined`,
       headers: {
         "Accept-Language": "zh-cn",
         "Accept-Encoding": "gzip, deflate, br",
@@ -166,7 +166,7 @@ function getCoupons(shareId = '') {
 function shareUnionCoupon() {
   return new Promise(resolve => {
     let opts = {
-      url: `https://api.m.jd.com/api?functionId=shareUnionCoupon&appid=u&_=${Date.now()}&loginType=2&body={%22unionActId%22:%2231134%22,%22actId%22:%22${$.actId}%22,%22platform%22:4,%22unionShareId%22:%22${$.shareCode}%22,%22d%22:%223C7eCOr%22,%22supportPic%22:2,%22supportLuckyCode%22:0,%22eid%22:%22${$.eid}%22}&client=apple&clientVersion=8.3.6`,
+      url: `https://api.m.jd.com/api?functionId=shareUnionCoupon&appid=u&_=${Date.now()}&loginType=2&body={%22unionActId%22:%2231134%22,%22actId%22:%22${$.actId}%22,%22platform%22:4,%22unionShareId%22:%22${$.shareCode}%22,%22d%22:%223KUlIt5%22,%22supportPic%22:2,%22supportLuckyCode%22:0,%22eid%22:%22${$.eid}%22}&client=apple&clientVersion=8.3.6`,
       headers: {
         "Accept-Language": "zh-cn",
         "Accept-Encoding": "gzip, deflate, br",
@@ -184,7 +184,7 @@ function shareUnionCoupon() {
           let res = $.toObj(data,data);
           if(typeof res == 'object'){
             if(res.code == 0 && res.data && res.data.shareUrl){
-              $.shareCode = res.data.shareUrl.match(/3C7eCOr\?s=([^&]+)/) && res.data.shareUrl.match(/3C7eCOr\?s=([^&]+)/)[1] || ''
+              $.shareCode = res.data.shareUrl.match(/3KUlIt5\?s=([^&]+)/) && res.data.shareUrl.match(/3KUlIt5\?s=([^&]+)/)[1] || ''
               console.log('分享码:'+$.shareCode)
               if($.shareCode) console.log(`以下账号会助力【京东账号${$.index}】${$.nickName || $.UserName}`)
             }
@@ -230,7 +230,7 @@ function getUrl1() {
 function getUrl() {
   return new Promise(resolve => {
     const options = {
-      url: `https://u.jd.com/3C7eCOr?s=${$.shareCode}`,
+      url: `https://u.jd.com/3KUlIt5?s=${$.shareCode}`,
       followRedirect:false,
       headers: {
         'Cookie': `${cookie} ${newCookie}`,
